c6b0b2f3961e24cb7f5fd586106730ff721721ba
Abraham Duno <tresduno@gmail.com>
2025-03-21 13:11:56 -0400

    ui 6

--- src/app/multi/page.tsx
+++ src/app/multi/page.tsx
@@ -1,11 +1,15 @@
+"use client"
 import { generateMetadata } from '../../../scripts/service'
 import styles from './page.module.css'
 import MultiPlayerPage from '@/dom/MultiPlayerPage'
+import { SessionProvider } from 'next-auth/react'
 
 export default async function MultiPlayer() {
   return (
     <main className="flex-col gap-2 flex-justify-center flex-align-center w-100vw h-100vh">
-      <MultiPlayerPage  />
+      <SessionProvider>
+        <MultiPlayerPage />
+      </SessionProvider>
     </main>
   )
 } 
