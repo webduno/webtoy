1e43fea861f8be548513d757cefb1491b686af8e
Abraham Duno <tresduno@gmail.com>
2025-03-29 21:40:10 -0400

    ui 1

--- src/dom/atom/game/BackgroundMusic.tsx
+++ src/dom/atom/game/BackgroundMusic.tsx
@@ -1,31 +1,33 @@
 "use client"
 
 import { useBackgroundMusic } from '@/scripts/contexts/BackgroundMusicContext';
+import { GameButton } from './GameButton';
 
 export function BackgroundMusic() {
   const { isPlaying, togglePlay } = useBackgroundMusic();
 
   return (
-    <button
-      className='pos-abs bottom-0 left-0 mb-100 ml-4 opaci-chov--50 pa-1  bord-r-100'
-      // onTouchStart={togglePlay}
+    <div
+      className='pos-abs bottom-0 left-0 mb-100 ml-4  '
       onClick={togglePlay}
       style={{
         zIndex: 9000,
         position: 'fixed',
       }}
     >
-      {!isPlaying ? (
-        <>
-          {/* music emoji */}
-          <span className='tx-lg'>🎵</span>
-        </>
-      ) : (
-        <>
-          {/* pause emoji */}
-          <span className='tx-lg'>⏸️</span>
-        </>
-      )}
-    </button>
+      <GameButton type="zeta" classOverride="bord-r-100 " >
+        {!isPlaying ? (
+          <>
+            {/* music emoji */}
+            <span className='tx-lg'>🔊</span>
+          </>
+        ) : (
+          <>
+            {/* pause emoji */}
+            <span className='tx-lg'>⏸️</span>
+          </>
+        )}
+      </GameButton>
+    </div>
   );
 } 
