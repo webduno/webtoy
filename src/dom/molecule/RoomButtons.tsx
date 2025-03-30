ff9ca784ad1d5cc120a673abfe3cd79c99b0cb72
Abraham Duno <tresduno@gmail.com>
2025-03-29 21:29:07 -0400

    ui 4

--- src/dom/molecule/RoomButtons.tsx
+++ src/dom/molecule/RoomButtons.tsx
@@ -102,9 +102,10 @@ export function RoomButtons({myip}: {myip: string}) {
         {loading === 'google' ? 'Logging in...' : 'Login with Google'}
       </button> */}
       
-      <GameButton type="delta" onClick={loginWithGoogle} classOverride="bord-r-100 mt-1" >
-        <div className='noselect px-4 py-1 tx-lg flex-row gap-2 flex-align-center flex-justify-center'>
-          <div className='bg-white bord-r-100 pt- px-1'>
+      {!session && (
+        <GameButton type="delta" onClick={loginWithGoogle} classOverride="bord-r-100 mt-1" >
+          <div className='noselect px-4 py-1 tx-lg flex-row gap-2 flex-align-center flex-justify-center'>
+            <div className='bg-white bord-r-100 pt- px-1'>
             <svg width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
               <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"/>
               <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"/>
@@ -115,6 +116,7 @@ export function RoomButtons({myip}: {myip: string}) {
           {loading === 'google' ? 'Logging in...' : 'Login with Google'}
         </div>
       </GameButton>
+      )}
     </div>
 
 
