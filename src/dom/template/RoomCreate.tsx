946f613b2fdf51ede204ef418391fcad966c4b93
Abraham Duno <tresduno@gmail.com>
2025-04-02 14:25:45 -0400

    username portal 4

--- src/dom/template/RoomCreate.tsx
+++ src/dom/template/RoomCreate.tsx
@@ -4,7 +4,35 @@ import Link from "next/link";
 import { RoomButtons } from "../molecule/RoomButtons";
 import { SessionProvider } from "next-auth/react";
 import { LandingLogo } from "../atom/logo/LandingLogo";
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+
 export function RoomCreate({myip}: {myip: string}) {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  useEffect(() => {
+    const username = searchParams.get('username');
+    const ref = searchParams.get('ref');
+
+    if (username && ref) {
+      localStorage.setItem('PLAYER_ID', username);
+      localStorage.setItem('REF_ID', ref);
+      const params = new URLSearchParams(searchParams.toString());
+      router.push(`/portals?${params.toString()}`);
+    } else if (username) {
+      localStorage.setItem('PLAYER_ID', username);
+      const params = new URLSearchParams(searchParams.toString());
+      router.push(`/portals?${params.toString()}`);
+    } else if (ref) {
+      localStorage.setItem('PLAYER_ID', ref);
+      const params = new URLSearchParams(searchParams.toString());
+      params.set('username', ref);
+      router.push(`/portals?${params.toString()}`);
+    }
+  }, [searchParams, router]);
+
   return (
     <SessionProvider>
       <div className="flex-col gap-2 flex-justify-center flex-align-center ">
